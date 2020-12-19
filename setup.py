575fe20c09d9b1f55a89f7b1c725b3ea9f56451f
LinfordDU <31502154+ZephyrDu@users.noreply.github.com>
2020-12-19 23:07:25 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,26 +1,15 @@
 from setuptools import setup, find_packages
 
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name='fgo-bot',
-    version='0.2',
+    version='0.5',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'opencv-python',
-        'numpy',
+        'opencv-python<=4.3.0.38',
+        'numpy<=1.17.5',
         'matplotlib'
     ],
-
-    author='willC',
-    author_email='will7101c@gmail.com',
-    description='An automated game bot for Fate Grand Order (CN).',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    keywords='fgo fate-grand-order auto bot',
-    url='https://github.com/will7101/fgo-bot',
     license='MIT',
     classifiers=[
         'Development Status :: 4 - Beta',
